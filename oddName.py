df66eecd73b53a656815baf9208039a933815aeb
jc444921 <xiting.jia@my.jcu.edu.au>
2016-12-09 20:00:58 +1000

    complete the file

--- oddName.py
+++ oddName.py
@@ -2,3 +2,12 @@ __author__ = 'jc444921'
 """
    Xiting Jia
 """
+valid = False
+while not valid:
+   name = input("Enter your name:")
+   if name != "" and not name.isspace():
+       valid=True
+
+l = len(name)
+for i in range(0,l,2):
+    print(name[i])
